73f246a671a2ea4625f88a4e424c236ecff49c15
zymotik <email@ian-b.com>
2023-04-24 10:33:39 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vuepic-vue-datepicker
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vuepic-vue-datepicker-i2x3hn)
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Playgdound from './components/Playground.vue';
+</script>
+
+<template>
+  <Playgdound />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
